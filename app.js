8b26f653e0684a41bc3b06643e40add42e7118b1
tinhonngTesting <tin315@hotmail.com>
2015-04-20 16:09:00 -0700

    add form submit and some two way bindings

--- app.js
+++ app.js
@@ -4,9 +4,19 @@
 var app = angular.module('myapp', []);
 
 app.controller('myController', function($scope){
-    $scope.users = [{
-        name: 'Tin', lat: 32.32, lon: 3567
-    }];
+    $scope.user = {};
+    $scope.users = [
+        {name: 'Tin', lat: 37.3382, lon: -121.8803},
+        {name: 'BP', lat: 37.34, lon: -121.9965},
+        {name: 'Mary', lat: 37.45, lon: -121.90}
+    ];
+    $scope.add = function(user){
+        if($scope.myForm.$valid) {
+            $scope.users.push(user);
+            $scope.user = angular.copy({});
+            $scope.myForm.$setPristine();
+        }
+    }
 });
 
 app.directive('googlemap', function(){
@@ -16,17 +26,45 @@ app.directive('googlemap', function(){
        replace: 'true',
        templateUrl: 'map.html',
        link: function(scope, elem, attrs){
-           var myLatlng = new google.maps.LatLng(scope.markers[0].lat,scope.markers[0].lon)
+           var myLatlng = new google.maps.LatLng(scope.markers[1].lat,scope.markers[1].lon);
            var mapProp = {
                center:myLatlng,
-               zoom:5,
-               mapTypeId:google.maps.MapTypeId.ROADMAP
+               zoom:10,
+               mapTypeId:google.maps.MapTypeId.ROADMAP,
+               scrollwheel: false
            };
            var map=new google.maps.Map(document.getElementById("mapArea"),mapProp);
-           var marker = new google.maps.Marker({
-               position: myLatlng,
-               map: map,
-               title: scope.markers[0].name
+
+           var markers = scope.markers;
+           if(markers){
+               markers.forEach(function(e){
+                   var marker = new google.maps.Marker({
+                       position: new google.maps.LatLng(e.lat,e.lon),
+                       map: map,
+                       title: e.name
+                   });
+               });
+           }
+           google.maps.event.addDomListener(document.getElementById('submit-btn'), 'click', function(){
+               var myLatlng = new google.maps.LatLng(scope.markers[1].lat,scope.markers[1].lon);
+               var mapProp = {
+                   center:myLatlng,
+                   zoom:10,
+                   mapTypeId:google.maps.MapTypeId.ROADMAP,
+                   scrollwheel: false
+               };
+               var map=new google.maps.Map(document.getElementById("mapArea"),mapProp);
+
+               var markers = scope.markers;
+               if(markers){
+                   markers.forEach(function(e){
+                       var marker = new google.maps.Marker({
+                           position: new google.maps.LatLng(e.lat,e.lon),
+                           map: map,
+                           title: e.name
+                       });
+                   });
+               }
            });
        }
    }
